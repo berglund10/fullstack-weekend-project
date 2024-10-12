e127baf6c9dcffcf403091bff8a68840631cad06
Anton Berglund <antonberglund_92@hotmail.com>
2024-10-12 15:25:16 +0200

    add eventlisteners to frontend

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,19 +1,39 @@
-const text = document.getElementById("fromJS") as HTMLParagraphElement;
+document.addEventListener('DOMContentLoaded', () => {
+    const input = document.getElementById("mobilePhoneNumber") as HTMLInputElement;
+    const form = document.getElementById("mobilePhoneNumberForm") as HTMLFormElement;
+    const paragraph = document.getElementById("messageFromBackend") as HTMLParagraphElement;
 
-if (text) {
-  callBackend(text);
-}
-
-async function callBackend(text: HTMLParagraphElement) {
-  try {
-    const result = await window.fetch("http://localhost:3000");
-    const message = await result.json();
-    text.innerText = message.message;
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      console.log(error.stack);
-      return;
+    if(form) {
+        form.addEventListener("submit", async (e) => {
+            e.preventDefault();
+            await callBackend(paragraph, input);
+        })
+    } else {
+        console.error("form not found");
     }
-    console.log("Unknown error:", JSON.stringify(error));
+})
+
+async function callBackend(text: HTMLParagraphElement, input: HTMLInputElement) {
+
+    const inputData = {
+        MobilePhoneNumber: input.value
+    }
+    try {
+      const result = await window.fetch("http://localhost:3000", {
+        method: "POST",
+        headers: {
+          "Content-Type": "text/plain",
+        },
+        body: JSON.stringify(inputData),
+      });
+      const message = await result.json();
+      text.innerText = message.message;
+
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        console.log(error.stack);
+        return;
+      }
+      console.log("Unknown error:", JSON.stringify(error));
+    }
   }
-}
