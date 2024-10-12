3bbee5608ba355e8b8a2986adde2441cf3c21445
Anton Berglund <antonberglund_92@hotmail.com>
2024-10-12 11:40:39 +0200

    add allow origin to backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import http, { IncomingMessage, ServerResponse } from 'node:http';
 const app = http.createServer((req: IncomingMessage, res: ServerResponse) => {
         res.writeHead(200, {
             "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "http://localhost:8080"
         });
         res.end(JSON.stringify({ message: "ok" }));
 })
