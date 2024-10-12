4eefeb36ab920719c010b1708c27bddb3db69f75
Anton Berglund <antonberglund_92@hotmail.com>
2024-10-12 13:08:13 +0200

    add check for letters in number

--- backend/src/phoneNumberValidator.test.ts
+++ backend/src/phoneNumberValidator.test.ts
@@ -3,47 +3,46 @@ import test from "node:test";
 import { validateNumber } from "./phoneNumberValidator";
 
 test("empty mobile number should return false", () => {
-    const result = validateNumber("");
-    deepEqual(result, false);
-})
+  const result = validateNumber("");
+  deepEqual(result, false);
+});
 
 test("should return false for a too short mobile number", () => {
-    const result = validateNumber("073400");
-    deepEqual(result, false);
-})
+  const result = validateNumber("073400");
+  deepEqual(result, false);
+});
 
 test("should return false for a too long mobile number", () => {
-    const result = validateNumber("0734001337123123");
-    deepEqual(result, false);
-})
+  const result = validateNumber("0734001337123123");
+  deepEqual(result, false);
+});
 
 test("should return false for incomplete mobile number format", () => {
-    const result = validateNumber("0784002141");
-    deepEqual(result, false);
-})
+  const result = validateNumber("0784002141");
+  deepEqual(result, false);
+});
 
-test.skip("should return false if mobile number contains letters", () => {
-    const result = validateNumber("073abc");
-    deepEqual(result, false);
-})
+test("should return false if mobile number contains letters", () => {
+  const result = validateNumber("073abc");
+  deepEqual(result, false);
+});
 
 test.skip("should return true for 0046 format", () => {
-    const result = validateNumber("0046704778899");
-    deepEqual(result, true);
-})
+  const result = validateNumber("0046704778899");
+  deepEqual(result, true);
+});
 
 test.skip("should return true for +46 format", () => {
-    const result = validateNumber("+46704778899")
-    deepEqual(result, true);
-})
+  const result = validateNumber("+46704778899");
+  deepEqual(result, true);
+});
 
 test.skip("should return true for 070/072/073/076/079", () => {
-    const result = validateNumber("0704778899");
-    deepEqual(result, true);
-})
+  const result = validateNumber("0704778899");
+  deepEqual(result, true);
+});
 
 test.skip("should allow spaces in mobile number", () => {
-    const result = validateNumber("070 477 88 99");
-    deepEqual(result, true);
-})
-
+  const result = validateNumber("070 477 88 99");
+  deepEqual(result, true);
+});
--- backend/src/phoneNumberValidator.ts
+++ backend/src/phoneNumberValidator.ts
@@ -2,15 +2,21 @@ export function validateNumber(mobilePhoneNumber: string) {
   if (mobilePhoneNumber.length === 0) {
     return false;
   }
-  if(mobilePhoneNumber.length < 10) {
+  if (mobilePhoneNumber.length < 10) {
     return false;
   }
-  if(mobilePhoneNumber.length > 10) {
+  if (mobilePhoneNumber.length > 10) {
     return false;
   }
 
-  if (!["0", "2", "3", "6", "9" ].includes(mobilePhoneNumber[2])) {
+  if (!["0", "2", "3", "6", "9"].includes(mobilePhoneNumber[2])) {
     return false;
-}
+  }
+
+  for(let i = 0; i < mobilePhoneNumber.length; i++) {
+    if(isNaN(Number(mobilePhoneNumber[i]))) {
+        return false;
+    }
+  }
   return true;
 }
