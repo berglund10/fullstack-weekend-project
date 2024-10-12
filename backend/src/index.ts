29f8e999837e6f90f577137e4413a866c74a9093
Anton Berglund <antonberglund_92@hotmail.com>
2024-10-12 11:07:09 +0200

    add http server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,12 @@
-console.log("Make sure typescript compiles");
+import http, { IncomingMessage, ServerResponse } from 'node:http';
+
+const app = http.createServer((req: IncomingMessage, res: ServerResponse) => {
+        res.writeHead(200, {
+            "Content-Type": "application/json",
+        });
+        res.end(JSON.stringify({ message: "ok" }));
+})
+
+app.listen(3000, () => {
+    console.log("app is listen on port 3000");
+});
