5d1a530af4f74d732c6abf99eeaaf6e3c947587e
Anton Berglund <antonberglund_92@hotmail.com>
2024-10-12 11:42:23 +0200

    update p element from fetch result

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,5 +1,19 @@
 const text = document.getElementById("fromJS") as HTMLParagraphElement;
 
 if (text) {
-  text.innerText = "Texten kommer från JS";
+  callBackend(text);
+}
+
+async function callBackend(text: HTMLParagraphElement) {
+  try {
+    const result = await window.fetch("http://localhost:3000");
+    const message = await result.json();
+    text.innerText = message.message;
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.log(error.stack);
+      return;
+    }
+    console.log("Unknown error:", JSON.stringify(error));
+  }
 }
