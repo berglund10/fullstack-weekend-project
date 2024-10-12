56a9963e797f643263a88c0ee4d4da79c4e5c6b4
Anton Berglund <antonberglund_92@hotmail.com>
2024-10-12 14:14:16 +0200

    add post functionality to the backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,43 @@
-import http, { IncomingMessage, ServerResponse } from 'node:http';
+import http, { IncomingMessage, ServerResponse } from "node:http";
+import { validateNumber } from "./phoneNumberValidator";
 
 const app = http.createServer((req: IncomingMessage, res: ServerResponse) => {
-        res.writeHead(200, {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "http://localhost:8080"
-        });
-        res.end(JSON.stringify({ message: "ok" }));
-})
+  if (req.method !== "POST") {
+    res.writeHead(405, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Method Not Allowed" }));
+    return;
+  }
+
+  res.writeHead(200, {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "http://localhost:8080",
+  });
+
+  let body = "";
+
+  req.on("data", (chunk) => {
+    body += chunk;
+  });
+
+  req.on("end", () => {
+    if (validateNumber(JSON.parse(body).MobilePhoneNumber)) {
+      res.end(
+        JSON.stringify({
+          message:
+            "Your mobile number has been successfully validated. Please expect a text message shortly",
+        }),
+      );
+      return;
+    }
+    res.end(
+      JSON.stringify({
+        message:
+          "Mobile number is invalid. Please ensure you have entered a correct number.",
+      }),
+    );
+  });
+});
 
 app.listen(3000, () => {
-    console.log("app is listen on port 3000");
+  console.log("app is listen on port 3000");
 });
